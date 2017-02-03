803bf317dcb5e4132456d8f3bc81476967b43215
eadr-fil <fildv79@gmail.com>
2017-02-03 12:38:38 +0200

    Solution created. Simple test Delete method.

---
+++ DinamicSqlBuilder/SqlBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinamicSqlBuilder
+{
+    /// <summary>
+    /// Builder command strings to SQL servers 
+    /// </summary>
+    public static class SqlBuilder
+    {
+        /// <summary>
+        /// Build string to delete data from table.
+        /// </summary>
+        /// <param name="TableName">Table name for delete items</param>
+        /// <returns></returns>
+        public static string Delete(string TableName)
+        {
+            return String.Format("DELETE FROM {0}", TableName);
+        }
+    }
+}
---
+++ DinamicSqlBuilder_UnitTests/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DinamicSqlBuilder;
+
+namespace DinamicSqlBuilder_UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Get_Delete_Sql_String()
+        {
+            //arrange
+            string tableName1 = "tableName1";
+            string tableName2 = "tableName2";
+            string tableName3 = "tableName3";
+            //action
+            string res1 = SqlBuilder.Delete(tableName1);
+            string res2 = SqlBuilder.Delete(tableName2);
+            string res3 = SqlBuilder.Delete(tableName3);            
+            //assert
+            Assert.AreEqual(res1, "DELETE FROM tableName1");
+            Assert.AreEqual(res2, "DELETE FROM tableName2");
+            Assert.AreEqual(res3, "DELETE FROM tableName3");
+            Assert.IsTrue(res3 == "DELETE FROM tableName3");
+        }
+    }
+}

bf132c4053c528919c7a398e220cf967459790a3
eadr-fil <fildv79@gmail.com>
2017-02-06 14:30:15 +0200

    Added Method for creatin Insert query and testfore it.

--- DinamicSqlBuilder/SqlBuilder.cs
+++ DinamicSqlBuilder/SqlBuilder.cs
@@ -1,24 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DinamicSqlBuilder
 {
     /// <summary>
-    /// Builder command strings to SQL servers 
+    /// static class for creating query strings to SQL servers 
     /// </summary>
     public static class SqlBuilder
     {
         /// <summary>
-        /// Build string to delete data from table.
+        ///     static method for creating string for delete data from table.
         /// </summary>
-        /// <param name="TableName">Table name for delete items</param>
+        /// <param name="tableName">Table name for delete items</param>
         /// <returns></returns>
-        public static string Delete(string TableName)
+        public static string Delete(string tableName)
         {
-            return String.Format("DELETE FROM {0}", TableName);
+            return String.Format("DELETE FROM {0}", tableName);
+        }
+        /// <summary>
+        ///     static method for creating query string for insert data to table.
+        /// </summary>
+        /// <param name="tableName">Table name for insert items</param>
+        /// <param name="itemsList">List of table items for insert</param>
+        /// <returns></returns>
+        public static string Insert(string tableName, object obj)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("INSERT INTO " + tableName.ToUpper() + " (");
+
+            //object o = Activator.CreateInstance(obj);
+            //dynamic d = obj;
+            //int i = d.id;
+
+            //Type type = typeof(objType);
+
+
+            PropertyInfo[] info = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var item in info)
+            {
+                sb.Append(item.Name + ", ");
+            }
+            sb.Remove(sb.Length - 2, 2);
+
+            sb.Append(@") VALUES (");
+            foreach (var item in info)
+            {
+                var propertyName = item.Name;
+                var propertyValue = obj.GetType().GetProperty(propertyName).GetValue(obj, null);
+
+                if (item.PropertyType == typeof(string))
+                {
+                    sb.Append("'" + propertyValue.ToString() + "', ");
+                }
+                else
+                {                    
+                    sb.Append(propertyValue.ToString() + ", ");
+                }
+            }
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append(@")");
+            return sb.ToString();
         }
     }
 }
--- DinamicSqlBuilder_UnitTests/UnitTests.cs
+++ DinamicSqlBuilder_UnitTests/UnitTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DinamicSqlBuilder;
+using System.Dynamic;
 
 namespace DinamicSqlBuilder_UnitTests
 {
@@ -17,12 +18,31 @@ namespace DinamicSqlBuilder_UnitTests
             //action
             string res1 = SqlBuilder.Delete(tableName1);
             string res2 = SqlBuilder.Delete(tableName2);
-            string res3 = SqlBuilder.Delete(tableName3);            
+            string res3 = SqlBuilder.Delete(tableName3);
             //assert
             Assert.AreEqual(res1, "DELETE FROM tableName1");
             Assert.AreEqual(res2, "DELETE FROM tableName2");
             Assert.AreEqual(res3, "DELETE FROM tableName3");
             Assert.IsTrue(res3 == "DELETE FROM tableName3");
         }
+
+
+        [TestMethod]
+        public void Get_Insert_Sql_String()
+        {
+            //arrange
+            string tableName = "MyTable";
+            MyItem mi = new MyItem { Id = 1, Name= "John"};         
+            //action
+            string res = SqlBuilder.Insert(tableName, mi);
+            //assert
+            Assert.AreEqual(res, "INSERT INTO MYTABLE (Id, Name) VALUES (1, 'John')");            
+        }
+    }
+
+    public class MyItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
